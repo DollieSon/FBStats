a15003f02fa50e94653e873b200304a4b8981da0
Ray Anthony <62559681+DollieSon@users.noreply.github.com>
2024-09-02 18:50:05 +0800

    solved parsing data to json

--- reader.py
+++ reader.py
@@ -3,6 +3,12 @@ import json
 PATH = "C:\\Users\\ray anthony\\Downloads\\FB Something\\FB Something\\your_facebook_activity\\messages\\inbox\\"
 PERSON_NAME = "Ray Anthony Dollison"
 
+BANNED_SUBSTR = [
+    "voted for",
+    "sent an attachment",
+    "sent a link"
+]
+
 def list_files(directory):
     file_list = []
     for root, dirs, files in os.walk(directory):
@@ -16,21 +22,30 @@ def list_files(directory):
         }
     }
 """
-def get_messages(messages,container):
+def get_messages(messages,container,PathName):
+
     for message in messages :
         if 'content' not in message.keys():
             continue
-        if message['sender_name'] not in container.keys():
-            container[message['sender_name']] = {}
+        sender_name = message['sender_name']
+        if sender_name not in container.keys():
+            container[sender_name] = {}
+        if PathName not in container[sender_name].keys():
+            container[sender_name][PathName] = {}
         time = message['timestamp_ms']
         content = message['content']
-        container[message['sender_name']][time] = content
+        container[sender_name][PathName][time] = content
 
 # Example usage:
 files = list_files(PATH)
 people = {
 
 }
+"""
+"Name" : {
+
+}
+"""
 for  file in files:
     # print(file)
     if file.endswith(".json"):
@@ -42,13 +57,17 @@ for  file in files:
         with open(file,'r') as jason:
             data = json.load(jason)
             #Count the people if 2 then 1v1 else GC
-            if len(data['participants']) == 2: # Normal Convo
-                for name in data['participants']:
-                    if name != PERSON_NAME:
-                        ChatName = name
-                        break
-            get_messages(data['messages'],people)
+            # if len(data['participants']) == 2: # Normal Convo
+            #     for name in data['participants']:
+            #         if name != PERSON_NAME:
+            #             ChatName = name
+            #             break
+            filtered = get_messages(data['messages'],people,ChatName)
+            # insert to people
 
+
+with open("Saved.json","w") as outfile:
+    json.dump(people,outfile)
             # need to insert person
             # print(data['participants'])
             # for message in messages:
@@ -56,14 +75,6 @@ for  file in files:
 
 
 
-"""
-[
-    "Chat_Name":[
-        [
-            "Timestamp" : "Content"
-        ]
-    ]
-]
-"""
+
 
 # print(files)
